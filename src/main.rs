151b3ea86de4346d9e2006a5575d695e127f6862
andi <andi@inventorum.com>
2015-04-05 21:09:01 +0200

    - first steps, including logging

---
+++ src/main.rs
@@ -0,0 +1,25 @@
+#[macro_use]
+extern crate log;
+extern crate fern;
+extern crate time;
+
+fn main(){
+	let logger_config = fern::DispatchConfig {
+	    format: Box::new(|msg: &str, level: &log::LogLevel, _location: &log::LogLocation| {
+	        // This is a fairly simple format, though it's possible to do more complicated ones.
+	        // This closure can contain any code, as long as it produces a String message.
+	        format!("[{}][{}] {}", time::now().strftime("%Y-%m-%d][%H:%M:%S").unwrap(), level, msg)
+	    }),
+	    // add a file-output: ``, fern::OutputConfig::file("output.log")``
+	    output: vec![fern::OutputConfig::stdout()],
+	    level: log::LogLevelFilter::Trace,
+	};
+
+	if let Err(e) = fern::init_global_logger(logger_config, log::LogLevelFilter::Trace) {
+	    panic!("Failed to initialize global logger: {}", e);
+	}
+
+	info!("Info message");
+	let x: i64 = 5;
+	info!("x is {}", x);
+}
