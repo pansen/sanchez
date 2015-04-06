08fa5058f88bb5f2358dc548d0ea9a32aa345269
andi <andi@inventorum.com>
2015-04-06 16:57:38 +0200

    - read and split the output of a command

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,7 @@ extern crate time;
 
 mod logging;
 use std::process::Command;
+use std::string::String;
 
 fn main(){
 	logging::setup_logging();
@@ -17,9 +18,19 @@ fn main(){
 }
 
 /// try a system command
+/// see:
+/// 	- http://stackoverflow.com/questions/26478009/running-an-external-process-in-rust
+/// 	- https://doc.rust-lang.org/core/result/
+///
+/// about strings in rust: http://stackoverflow.com/a/24159933
 fn cmd(cmd: &str) {
-	let output = Command::new(cmd).arg("-la").output().unwrap_or_else(|e| {
-	  panic!("failed to execute process: {}", e)
-	});
-	info!("output: {}", output.stdout[0]);
+	let process = Command::new(cmd).arg("-la").output().ok().expect("Failed to execute");
+	let stdout = String::from_utf8(process.stdout).ok().expect("Failed to read");
+
+	info!("{}", stdout);
+
+	let split: Vec<&str> = stdout.split('\n').collect();
+	for i in split {
+		info!("     {}", i);
+	}
 }
